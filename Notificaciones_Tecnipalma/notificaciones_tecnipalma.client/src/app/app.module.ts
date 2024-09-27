75799b479c1244fed22afcda90fe55f53c4940ed
pudin11 <je.beltranc1@uniandes.edu.co>
2024-09-26 21:05:18 -0500

    Eliminado forecast, y pantalla de login cagando

--- Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/app.module.ts
+++ Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'; //importarFormsModule//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Login/login.component';
@@ -11,7 +11,7 @@ import { LoginComponent } from './Login/login.component';
     AppComponent, LoginComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule,
+    BrowserModule, FormsModule, HttpClientModule,
     AppRoutingModule
   ],
   providers: [],

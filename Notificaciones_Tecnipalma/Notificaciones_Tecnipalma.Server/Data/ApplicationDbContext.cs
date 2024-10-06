24f2be8ad1cfab43b329cab91198c82a1b040cf6
pudin11 <je.beltranc1@uniandes.edu.co>
2024-10-06 14:07:41 -0500

    Inicio de sesion exitoso en el back.

--- Notificaciones_Tecnipalma/Notificaciones_Tecnipalma.Server/Data/ApplicationDbContext.cs
+++ Notificaciones_Tecnipalma/Notificaciones_Tecnipalma.Server/Data/ApplicationDbContext.cs
@@ -8,6 +8,20 @@ namespace LoginAPI.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Usuarios> Usuarios { get; set; }
+
+        //Configuración del modelo
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Configura 'NombreUsuario' como clave primaria
+            modelBuilder.Entity<Usuarios>()
+                .HasKey(u => u.Usuario); // Aquí se define la clave primaria
+
+            // Puedes agregar más configuraciones de entidades aquí si es necesario
+        }
     }
+
+    
 }
 

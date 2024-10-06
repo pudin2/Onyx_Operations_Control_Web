8af6f58f2063c45f768946b452816d10d59faedd
pudin11 <je.beltranc1@uniandes.edu.co>
2024-10-06 15:29:25 -0500

    Autenticacin completa, usando el front.

--- Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/auth.service.ts
+++ Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/auth.service.ts
@@ -13,6 +13,10 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   login(username: string, password: string): Observable<any> {
-    return this.http.post<any>(this.apiUrl, { username, password });
+    const body = {
+      usuario: username, // Aquí ajustamos el nombre del campo
+      contraseña: password
+    };
+    return this.http.post<any>(this.apiUrl, body);
   }
 }

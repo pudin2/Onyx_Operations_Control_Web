9b595b35d85ff0067e1f23ac7babf5c35cc04343
pudin11 <je.beltranc1@uniandes.edu.co>
2024-10-07 12:27:11 -0500

    Ya no se duplica el login, cambio de pagina al autenticarse. Menu duplicado.

--- Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/app-routing.module.ts
+++ Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/app-routing.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Login/login.component';
 import { MenuComponent } from './Menu/menu.component';
+import { MainLayoutComponent } from './layouts/main_layout/main-layout.component';
 //import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }, // Redirige a la página de login por defecto
-  { path: 'menu', component: MenuComponent },  // Ruta para la página de login
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: '**', redirectTo: '/login' }// Ruta para la página de menú
+  { path: 'login', component: LoginComponent },// Redirige a la página de login por defecto
+  {
+    path: '',
+    component: MainLayoutComponent, // Este es el layout que incluye el menú
+    children: [
+      { path: 'menu', component: MenuComponent },
+      { path: '', redirectTo: '/login', pathMatch: 'full' },
+      { path: '**', redirectTo: '/login' }// Ruta para la página de menú
+    ]
+  },
+  { path: '**', redirectTo: '/login' }
 ];
 
 

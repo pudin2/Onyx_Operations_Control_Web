4bb5c6d024b49a15a889e8056a176be72758509e
JuliGasca <j.gasca@uniandes.edu.com>
2024-10-07 16:31:35 -0500

    menu con cambios en el front

--- Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/Menu/menu.component.ts
+++ Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/Menu/menu.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
+  standalone: true,
+  imports: [MatButtonModule, MatMenuModule],
 })
 export class MenuComponent {
 

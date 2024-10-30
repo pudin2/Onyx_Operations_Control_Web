e30043dd233378ab3512af55ae19d266a00e8b82
JuliGasca <j.gasca@uniandes.edu.com>
2024-10-29 21:54:02 -0500

    Save model corregido

--- Notificaciones_Tecnipalma/Notificaciones_Tecnipalma.Server/Models/SaveModel.cs
+++ Notificaciones_Tecnipalma/Notificaciones_Tecnipalma.Server/Models/SaveModel.cs
@@ -20,7 +20,7 @@ public class SaveDataRequest2
 {
     public string? CodInventario { get; set; }
     public decimal? Cant { get; set; }
-    public long? DetCotizacion_Id { get; set; 
+    public long? DetCotizacion_Id { get; set; }
     public string? Inventario_ID { get; set; }
     public int? Unidad_Id { get; set; }
     public string? Estado { get; set; }

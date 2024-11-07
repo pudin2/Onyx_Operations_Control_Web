cccd881d451e3cfb10e8949d4483e3131279186d
JuliGasca <j.gasca@uniandes.edu.com>
2024-11-06 19:15:37 -0500

    joseph arreglos

--- Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/Servicios/auth.service.ts
+++ Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/Servicios/auth.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl = 'https://localhost:5001/api/auth/login';
+  private tokenKey = 'authToken'; // Clave para almacenar el token en localStorage
 
   constructor(private http: HttpClient) { }
 
@@ -15,6 +16,26 @@ export class AuthService {
       usuario: username, // Aquí ajustamos el nombre del campo
       contraseña: password
     };
-    return this.http.post<any>(this.apiUrl, body);
+    return this.http.post<any>(this.apiUrl, body).pipe(
+      tap(response => {
+        if (response && response.token) {
+          localStorage.setItem(this.tokenKey, response.token);
+        }
+      })
+    );
+  }
+  isAuthenticated(): boolean {
+    return !!localStorage.getItem(this.tokenKey); // Devuelve true si hay un token
+  }
+
+  // Método para cerrar sesión
+  logout(): void {
+    localStorage.removeItem(this.tokenKey); // Elimina el token de localStorage
+  }
+
+  // Método para obtener el token de autenticación
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
   }
 }
+
--- Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/app-routing.module.ts
+++ Notificaciones_Tecnipalma/notificaciones_tecnipalma.client/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './Login/login.component';
 import { MenuComponent } from './Menu/menu.component';
 import { MainLayoutComponent } from './layouts/main_layout/main-layout.component';
 import { CargaNotiComponent } from './Produccion/Noti/CargaNoti.component'
-import { NotiComponent } from './Produccion/Noti/Noti.component'; 
+import { NotiComponent } from './Produccion/Noti/Noti.component';
+import { AuthGuard } from './Servicios/auth.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },// Redirige a la página de login por defecto
@@ -12,9 +13,9 @@ const routes: Routes = [
     path: '',
     component: MainLayoutComponent, // Este es el layout que incluye el menú
     children: [
-      { path: 'menu', component: MenuComponent },
-      { path: 'Produccion/noti', component: CargaNotiComponent },
-      { path: 'produccion/noti/:Id', component: NotiComponent },
+      { path: 'menu', component: MenuComponent, canActivate: [AuthGuard] },
+      { path: 'Produccion/noti', component: CargaNotiComponent, canActivate: [AuthGuard] },
+      { path: 'produccion/noti/:Id', component: NotiComponent, canActivate: [AuthGuard] },
       { path: '', redirectTo: '/login', pathMatch: 'full' },
       { path: '**', redirectTo: '/login' }// Ruta para la página de menú
     ]
